1ccaf01822cd539164318e26efdcc4ff180ae20e
Nicolas DUBIEN <github@dubien.org>
2020-07-07 19:59:06 +0200

    fix 3?

--- test-bundle/esm/index.test.js
+++ test-bundle/esm/index.test.js
@@ -1,17 +1,6 @@
 import { testProp } from 'ava-fast-check';
 import fc from 'fast-check';
 
-console.log({
-  testProp,
-  fc,
-  string: fc.string
-});
-
-console.log({
-  typeof_string: typeof fc.string,
-  ctor_string: fc.string__proto__.constructor.name
-});
-
 // for all a, b, c strings
 // b is a substring of a + b + c
 testProp('should detect the substring', [fc.string(), fc.string(), fc.string()], (t, a, b, c) => {
