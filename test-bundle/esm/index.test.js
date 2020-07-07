678ae3397c4e7b408d21c34dc06b8d97ad46e960
Nicolas DUBIEN <github@dubien.org>
2020-07-07 19:47:26 +0200

    debug?

--- test-bundle/esm/index.test.js
+++ test-bundle/esm/index.test.js
@@ -1,5 +1,7 @@
 import { testProp, fc } from 'ava-fast-check';
 
+console.log({ testProp, fc });
+
 // for all a, b, c strings
 // b is a substring of a + b + c
 testProp('should detect the substring', [fc.string(), fc.string(), fc.string()], (t, a, b, c) => {
